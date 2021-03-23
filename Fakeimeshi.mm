10f78361ca4f9fffbd602ba1d17a10bc1945163b
fermosh <fermosh@gmail.com>
2021-03-23 17:54:31 -0600

    Create Fakeimeshi.mm

---
+++ Fakeimeshi.mm
@@ -0,0 +1,18 @@
+# Fakeimeshi 
+
+* 1 paquete de bits de coliflor 
+* 1/4 taza de cebollín picado 
+* 2 huevos
+* 4 cucharadas de aceite de cacahuate
+* Gengibre 
+* Salsa de soya
+* 1 calabacita
+* (Las verduras que quieras agregar picadas en cubitos de 5mm)
+
+1. Sofreír las verduras en un wok con la mitad del aceite (sin el cebollín ni el gengibre)
+2. Agregar los bits y freír por 5 min
+3. Hacer hacia las orillas el contenido de wok para dejar un espacio vacío al centro
+4. Agregar al centro del wok el aceite restante y los huevos batidos
+5. Mezclar y agregar salsa de soya para dar color y sabor
+6. Agregar el cebollín y gengibre rallado
+7. Terminar de integrar y agradecerle a lego
